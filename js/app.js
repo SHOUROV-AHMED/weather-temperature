d27ee6be529331ea2eb225c059e9668f18b537ed
ShuvoRyuk <shourova783@gmail.com>
2021-09-03 15:45:40 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const API_KEY='fd155f233168c86f868a453c7c4cabc2'
+
+const searchTemperature=()=>{
+    const city = document.getElementById('city-name').value;
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+
+}
+
+ const setInnerText = (id,text)=>{
+     document.getElementById(id).innerText = text
+ }
+const displayTemperature = temperature =>{
+   setInnerText('city',temperature.name)
+   setInnerText('temperature',temperature.main.temp)
+    
+   setInnerText('temp',temperature.weather[0].main);
+    
+    //set weather icon
+    const url =`http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src',url);
+
+}
